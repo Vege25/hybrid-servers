7fa7f07ee9cecc550a527c039d57735a70269dcf
VeetiSo <v.sorakivi@gmail.com>
2024-01-24 14:18:07 +0200

    tag queries

--- graphql-server/src/api/resolvers/mediaResolver.ts
+++ graphql-server/src/api/resolvers/mediaResolver.ts
@@ -1,9 +1,22 @@
-import {fetchAllMedia} from '../models/mediaModel';
+import {
+  fetchAllMedia,
+  fetchMediaById,
+  fetchMediaByTag,
+} from '../models/mediaModel';
 
 export default {
   Query: {
     mediaItems: async () => {
       return await fetchAllMedia();
+    },
+    mediaItem: async (_parent: undefined, args: {media_id: string}) => {
+      console.log(args);
+      const id = Number(args.media_id); // Typescript constructor changes string to number
+      return await fetchMediaById(id);
+    },
+    mediaItemsByTag: async (_parent: undefined, args: {tag: string}) => {
+      console.log(args);
+      return await fetchMediaByTag(args.tag);
     },
   },
 };
