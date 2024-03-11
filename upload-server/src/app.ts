b497d88bca4e0e6c9855f9e70744172ecfd947ba
VeetiSo <v.sorakivi@gmail.com>
2024-03-11 16:34:17 +0200

    upload

--- upload-server/src/app.ts
+++ upload-server/src/app.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
+import path from 'path';
 
 import {notFound, errorHandler} from './middlewares';
 import api from './api';
@@ -10,17 +11,17 @@ import {MessageResponse} from '../hybrid-types/MessageTypes';
 
 const app = express();
 
+// Middleware
 app.use(morgan('dev'));
-app.use(
-  helmet({
-    crossOriginResourcePolicy: false,
-  }),
-);
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.use('/uploads', express.static('uploads'));
+// Serve static files from the 'uploads' directory
+const uploadsPath = path.join(__dirname, 'uploads');
+app.use('/uploads', express.static(uploadsPath));
 
+// Routes
 app.get<{}, MessageResponse>('/', (req, res) => {
   res.json({
     message: 'API location: api/v1',
@@ -29,6 +30,7 @@ app.get<{}, MessageResponse>('/', (req, res) => {
 
 app.use('/api/v1', api);
 
+// Error handling middleware
 app.use(notFound);
 app.use(errorHandler);
 
